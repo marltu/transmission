70cf99a4f83507853565bf0c458d302aac5da6e4
Juliusz Chroboczek <jch@pps.jussieu.fr>
2011-01-11 22:40:54 +0100

    Implement outgoing uTP connections.

--- libtransmission/peer-mgr.c
+++ libtransmission/peer-mgr.c
@@ -3573,7 +3573,8 @@ initiateConnection( tr_peerMgr * mgr, Torrent * t, struct peer_atom * atom )
                                &atom->addr,
                                atom->port,
                                t->tor->info.hash,
-                               t->tor->completeness == TR_SEED );
+                               t->tor->completeness == TR_SEED,
+                               0 );
 
     if( io == NULL )
     {
